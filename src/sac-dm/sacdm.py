4da812cbd620d257448d9ef73a41d4c46bce95ff
NthnR <nathan.rtl.pb@gmail.com>
2023-05-08 12:01:24 -0300

    Plotando graficos de treino e teste.

--- src/sac-dm/sacdm.py
+++ src/sac-dm/sacdm.py
@@ -225,12 +225,142 @@ def test(file1, file2):
 
 	return 0
 
+def plotSAC_AM_DM():
+	N = int(sys.argv[3])
+
+	#Abrindo arquivos
+	F0 = np.genfromtxt( "../../files/drone_signals/accel_80_F0.csv", delimiter=';', names=['x','y','z','s','t'])
+	F6 = np.genfromtxt( "../../files/drone_signals/accel_80_F6_v1.csv", delimiter=';', names=['x','y','z','s','t'])
+	F14 = np.genfromtxt( "../../files/drone_signals/accel_80_F14.csv", delimiter=';', names=['x','y','z','s','t'])
+	F22 = np.genfromtxt( "../../files/drone_signals/accel_80_F22.csv", delimiter=';', names=['x','y','z','s','t'])
+
+	#Extraindo eixos
+	F0_x = F0['x'].reshape(-1)
+	F0_y = F0['y'].reshape(-1)
+	F0_z = F0['z'].reshape(-1)
+
+	F6_x = F6['x'].reshape(-1)
+	F6_y = F6['y'].reshape(-1)
+	F6_z = F6['z'].reshape(-1)
+
+	F14_x = F14['x'].reshape(-1)
+	F14_y = F14['y'].reshape(-1)
+	F14_z = F14['z'].reshape(-1)
+
+	F22_x = F22['x'].reshape(-1)
+	F22_y = F22['y'].reshape(-1)
+	F22_z = F22['z'].reshape(-1)
+
+	#Obtendo SAC_DM
+	sac_dm_F0_x = sac_dm(F0_x, N)
+	sac_dm_F0_y = sac_dm(F0_y, N)
+	sac_dm_F0_z = sac_dm(F0_z, N)
+
+	sac_dm_F6_x = sac_dm(F6_x, N)
+	sac_dm_F6_y = sac_dm(F6_y, N)
+	sac_dm_F6_z = sac_dm(F6_z, N)
+
+	sac_dm_F14_x = sac_dm(F14_x, N)
+	sac_dm_F14_y = sac_dm(F14_y, N)
+	sac_dm_F14_z = sac_dm(F14_z, N)
+
+	sac_dm_F22_x = sac_dm(F22_x, N)
+	sac_dm_F22_y = sac_dm(F22_y, N)
+	sac_dm_F22_z = sac_dm(F22_z, N)
+
+	#Obtendo SAC_AM
+	sac_am_F0_x = sac_am(F0_x, N)
+	sac_am_F0_y = sac_am(F0_y, N)
+	sac_am_F0_z = sac_am(F0_z, N)
+
+	sac_am_F6_x = sac_am(F6_x, N)
+	sac_am_F6_y = sac_am(F6_y, N)
+	sac_am_F6_z = sac_am(F6_z, N)
+
+	sac_am_F14_x = sac_am(F14_x, N)
+	sac_am_F14_y = sac_am(F14_y, N)
+	sac_am_F14_z = sac_am(F14_z, N)
+
+	sac_am_F22_x = sac_am(F22_x, N)
+	sac_am_F22_y = sac_am(F22_y, N)
+	sac_am_F22_z = sac_am(F22_z, N)
+
+	#Removendo ultima amostra
+	sac_dm_F0_x.pop()
+	sac_dm_F0_y.pop()
+	sac_dm_F0_z.pop()
+	
+	sac_dm_F6_x.pop()
+	sac_dm_F6_y.pop()
+	sac_dm_F6_z.pop()
+
+	sac_dm_F14_x.pop()
+	sac_dm_F14_y.pop()
+	sac_dm_F14_z.pop()
+
+	sac_dm_F22_x.pop()
+	sac_dm_F22_y.pop()
+	sac_dm_F22_z.pop()
+
+	sac_am_F0_x.pop()
+	sac_am_F0_y.pop()
+	sac_am_F0_z.pop()
+	
+	sac_am_F6_x.pop()
+	sac_am_F6_y.pop()
+	sac_am_F6_z.pop()
+
+	sac_am_F14_x.pop()
+	sac_am_F14_y.pop()
+	sac_am_F14_z.pop()
+
+	sac_am_F22_x.pop()
+	sac_am_F22_y.pop()
+	sac_am_F22_z.pop()
+
+	# # Criando e plotando graficos para o treinamento de SAC-AM
+
+	# fig_SAC_AM_T, (ax1_SAC_AM_X_T, ax2_SAC_AM_Y_T, ax3_SAC_AM_Z_T) = plt.subplots(3)
+	# util.showTreinamento([sac_am_F0_x, sac_am_F0_y, sac_am_F0_z], "SAC-AM", fig_SAC_AM_T, [ax1_SAC_AM_X_T, ax2_SAC_AM_Y_T, ax3_SAC_AM_Z_T])
+
+	# # Criando e plotando graficos para o treinamento de SAC-DM
+
+	# fig_SAC_DM_T, (ax1_SAC_DM_X_T, ax2_SAC_DM_Y_T, ax3_SAC_DM_Z_T) = plt.subplots(3)
+	# util.showTreinamento([sac_dm_F0_x, sac_dm_F0_y, sac_dm_F0_z], "SAC-DM", fig_SAC_DM_T, [ax1_SAC_DM_X_T, ax2_SAC_DM_Y_T, ax3_SAC_DM_Z_T])
+
+
+	# # Plotando em uma unica figura, todos os graficos
+
+	# util.showSAC_figUnico([[sac_am_F0_x, sac_am_F0_y, sac_am_F0_z], [sac_am_F6_x, sac_am_F6_y, sac_am_F6_z], [sac_am_F14_x, sac_am_F14_y, sac_am_F14_z],
+	# 						[sac_am_F22_x, sac_am_F22_y, sac_am_F22_z]], "SAC-AM")
+
+	# util.showSAC_figUnico([[sac_dm_F0_x, sac_dm_F0_y, sac_dm_F0_z], [sac_dm_F6_x, sac_dm_F6_y, sac_dm_F6_z], [sac_dm_F14_x, sac_dm_F14_y, sac_dm_F14_z],
+	# 						[sac_dm_F22_x, sac_dm_F22_y, sac_dm_F22_z]], "SAC-DM")
+
+
+
+	# Plotando graficos de forma individual
+
+	# # 								SAC-AM
+	util.showSAC([sac_am_F0_x, sac_am_F6_x, sac_am_F14_x, sac_am_F22_x], "SAC-AM: Eixo X")
+	util.showSAC([sac_am_F0_y, sac_am_F6_y, sac_am_F14_y, sac_am_F22_y], "SAC-AM: Eixo Y")
+	util.showSAC([sac_am_F0_z, sac_am_F6_z, sac_am_F14_z, sac_am_F22_z], "SAC-AM: Eixo Z")
+
+	# # 								SAC-DM
+	util.showSAC([sac_dm_F0_x, sac_dm_F6_x, sac_dm_F14_x, sac_dm_F22_x], "SAC-DM: Eixo X")
+	util.showSAC([sac_dm_F0_y, sac_dm_F6_y, sac_dm_F14_y, sac_dm_F22_y], "SAC-DM: Eixo Y")
+	util.showSAC([sac_dm_F0_z, sac_dm_F6_z, sac_dm_F14_z, sac_dm_F22_z], "SAC-DM: Eixo Z")
+
+	plt.show()
+
+	return 0
 
 #********* Main ********
 filename = sys.argv[1]
 filename2 = sys.argv[2]
 
-test(filename, filename2)
+# test(filename, filename2)
+plotSAC_AM_DM()
 
 
 
--- src/sac-dm/util.py
+++ src/sac-dm/util.py
@@ -39,3 +39,172 @@ def show(dataset, title):
 
 	#plt.show()
 	return 1
+
+def treinamentoMetade(dataset, title, fig, ax):
+	
+	plt.ylabel(title) 
+	plt.xlabel('Time (ms)')
+	
+	colors = list(mcolors.CSS4_COLORS) 
+
+	media_dataset = media_sac(dataset, 0, round(len(dataset)/2))
+	desv_dataset = desvio_sac(dataset, 0, round(len(dataset)/2))
+
+	aux_desv = np.zeros(len(dataset))
+	aux_desv[round((len(dataset))/2)] = desv_dataset
+
+	x = np.arange(len(dataset))
+	y = np.zeros(len(dataset))
+	y = np.full_like(y, media_dataset)
+
+	ax.plot(x,y,color=colors[10], label = "Média da primeira metade do SAC F0")
+
+	for j in range(len(dataset)):
+
+		if(aux_desv[j] != 0):			
+			ax.errorbar(j,media_dataset,yerr = aux_desv[j], color = colors[20],marker='s', capsize=2, markersize=4, linewidth=1, linestyle='--')
+
+	ax.fill_between(x, media_dataset - desv_dataset, media_dataset + desv_dataset, alpha = 0.2, label = "Desvio Padrão da primeira metade do Arquivo F0")
+
+
+def treinamentoCompleto(dataset, title, fig, ax):
+	
+	aux = title.split(':',1)
+	plt.ylabel(aux[0]) 
+	plt.xlabel('Time (ms)')
+	
+
+	ax.set_title(title)  
+	colors = list(mcolors.CSS4_COLORS) 
+
+	media_dataset = media_sac(dataset, 0, round(len(dataset)/2))
+	desv_dataset = desvio_sac(dataset, 0, round(len(dataset)/2))
+
+	aux_desv = np.zeros(len(dataset))
+	aux_desv[round((len(dataset))/2)] = desv_dataset
+
+	x = np.arange(len(dataset))
+	y = np.zeros(len(dataset))
+	y = np.full_like(y, media_dataset)
+
+	ax.plot(x,y,color=colors[10], label = "Média do Arquivo F0")
+
+
+	plt.xlim(right = (len(dataset)))
+
+	for j in range(len(dataset)):
+
+		if(aux_desv[j] != 0):			
+			ax.errorbar(j,media_dataset,yerr = aux_desv[j], color = colors[20],marker='s', capsize=2, markersize=4, linewidth=1, linestyle='--')
+
+	ax.fill_between(x, media_dataset - desv_dataset, media_dataset + desv_dataset, alpha = 0.2, label = "Desvio Padrão do Arquivo F0")
+
+
+
+
+def testagem(dataset, title, fig, ax, color):
+
+	colors = list(mcolors.CSS4_COLORS) 
+	ax.plot(dataset,color=colors[color], label = title)
+	
+def showTreinamento(dataset, title, fig, ax):
+
+	fig.suptitle('Treinamento')
+
+	#				Eixo X
+	auxT = title + ": Eixo X"
+	ax[0].set_title(auxT)
+	ax[0].set(ylabel = title)
+	dataset_treinamento = amostragem_sac(dataset[0], 0, (round(len(dataset[0])/2) + 1))
+	treinamentoMetade(dataset_treinamento, title, fig, ax[0])
+	dataset_teste = amostragem_sac(dataset[0], round(len(dataset[0])/2), len(dataset[0]) )
+	testagem(dataset_teste, "Segunda metade do arquivo F0", fig, ax[0], 11)
+	ax[0].legend(loc = 'lower left')
+
+	#				Eixo Y
+	auxT = title + ": Eixo Y"
+	ax[1].set_title(auxT)
+	ax[1].set(ylabel = title)
+	dataset_treinamento = amostragem_sac(dataset[1], 0, (round(len(dataset[1])/2) + 1))
+	treinamentoMetade(dataset_treinamento, title, fig, ax[1])
+	dataset_teste = amostragem_sac(dataset[1], round(len(dataset[1])/2), len(dataset[1]) )
+	testagem(dataset_teste, "Segunda metade do arquivo F0", fig, ax[1], 12)
+	ax[1].legend(loc = 'upper right')
+
+	#				Eixo Z
+	auxT = title + ": Eixo Z"
+	ax[2].set_title(auxT)
+	ax[2].set(ylabel = title)
+	dataset_treinamento = amostragem_sac(dataset[2], 0, (round(len(dataset[2])/2) + 1))
+	treinamentoMetade(dataset_treinamento, title, fig, ax[2])
+	dataset_teste = amostragem_sac(dataset[2], round(len(dataset[2])/2), len(dataset[2]) )
+	testagem(dataset_teste, "Segunda metade do arquivo F0", fig, ax[2], 13)
+	ax[2].legend(loc = 'upper right')
+
+def showSAC_figUnico(dataset, title):
+
+	# # Criando graficos base
+	fig, (ax1_X, ax2_Y, ax3_Z) = plt.subplots(3)
+	treinamentoCompleto(dataset[0][0], "Eixo X", fig, ax1_X)
+	treinamentoCompleto(dataset[0][1], "Eixo Y", fig, ax2_Y)
+	treinamentoCompleto(dataset[0][2], "Eixo Z", fig, ax3_Z)
+
+	fig.suptitle(title)
+
+	ax1_X.set(ylabel = title)
+	ax2_Y.set(ylabel = title)
+	ax3_Z.set(ylabel = title)
+
+	# # Plotar os eixos nos gráficos base
+	# #							SAC_DM
+	testagem(dataset[1][1], 'Arquivo F6', fig, ax1_X, 11)
+	testagem(dataset[1][1], 'Arquivo F14', fig, ax1_X, 12)
+	testagem(dataset[1][2], 'Arquivo F22', fig, ax1_X, 13)
+	ax1_X.legend(loc='lower right')
+
+
+	testagem(dataset[2][0], 'Arquivo F6', fig, ax2_Y, 11)
+	testagem(dataset[2][1], 'Arquivo F14', fig, ax2_Y, 12)
+	testagem(dataset[2][2], 'Arquivo F22', fig, ax2_Y, 13)
+	ax2_Y.legend(loc='upper left')
+
+
+	testagem(dataset[3][0], 'Arquivo F6', fig, ax3_Z, 11)
+	testagem(dataset[3][1], 'Arquivo F14', fig, ax3_Z, 12)
+	testagem(dataset[3][2], 'Arquivo F22', fig, ax3_Z, 13)
+	ax3_Z.legend(loc='lower right')
+
+def showSAC(dataset, title):
+
+	fig, ax = plt.subplots()
+
+	treinamentoCompleto(dataset[0],title, fig, ax)
+
+	testagem(dataset[1], 'Arquivo F6', fig, ax, 11)
+	testagem(dataset[2], 'Arquivo F14', fig, ax, 12)
+	testagem(dataset[3], 'Arquivo F22', fig, ax, 13)
+	ax.legend(loc='lower right')
+
+
+def media_sac(dataset,inicio,fim):
+
+	media = np.average(dataset[inicio:fim])
+
+	return media
+
+def variancia_sac(dataset,inicio,fim):
+
+	variancia = np.var(dataset[inicio:fim])
+
+	return variancia
+
+def desvio_sac(dataset, inicio, fim):
+
+	desvio_padrao = np.std(dataset[inicio:fim])
+
+	return desvio_padrao
+
+def amostragem_sac(dataset, inicio, fim):
+
+	return dataset[inicio:fim]
+
